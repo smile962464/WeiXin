7f19b034275c20eadf54f075f617c319030e2feb
Peterinor <tangyutianbulao@qq.com>
2013-08-31 14:59:41 +0800

    Signed-off-by: Peterinor <tangyutianbulao@qq.com>

---
+++ Weixin/WinXin.Test/MessageTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WinXin.Test
+{
+	[TestClass]
+	public class MessageTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
