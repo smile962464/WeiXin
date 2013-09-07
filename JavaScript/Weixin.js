be3806bef4b4ac8e997c4abb98e5b2a62d71b9ba
Peterinor <tangyutianbulao@qq.com>
2013-09-07 15:22:31 +0800

    t

---
+++ JavaScript/Weixin.js
@@ -0,0 +1,41 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Author: 禹
+ * Date: 13-9-7
+ * Time: 下午3:07
+ * File:
+ * Description:
+ */
+
+Winxin = {};
+
+
+Winxin.Utities = {
+    getTimeTicks: function (dt) {
+
+    },
+    getTime: function (ticks) {
+
+    },
+    log: function () {
+
+    },
+    getAccessToken: function (appid, appsecret) {
+
+    },
+    addMenuFromFile: function (accessToken, path) {
+
+    },
+    addMenu: function (accessToken, menu) {
+
+    },
+    getMenu: function (accessToken) {
+
+    },
+    deleteMenu: function (accessToken) {
+
+    }
+
+};
+
+
